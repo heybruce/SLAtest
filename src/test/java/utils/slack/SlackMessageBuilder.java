c8f1b9a1cb53cacd917f31a1c534a99aab89010c
Bruce Liao <bruce.liao@solera.com>
2019-02-15 15:23:19 +0800

    added country info to slack message

--- src/test/java/utils/slack/SlackMessageBuilder.java
+++ src/test/java/utils/slack/SlackMessageBuilder.java
@@ -5,13 +5,14 @@ import org.testng.ITestResult;
 
 public class SlackMessageBuilder {
 
-    String alertMessage;
+    private String alertMessage;
 
     public void composeAlertMessage(ITestResult result) {
         JSONObject message = new JSONObject();
         message.put("text", "Test failed 3 times in a row. Please verify for potential issue.\nTest name: "
                 + result.getMethod().getMethodName()
                 + "\nEnvironment: " + result.getTestContext().getCurrentXmlTest().getLocalParameters().get("env")
+                + "\nCountry: " + result.getTestContext().getCurrentXmlTest().getLocalParameters().get("country")
                 + "\nBrowser: " + result.getTestContext().getCurrentXmlTest().getLocalParameters().get("browser"));
 
         alertMessage = message.toJSONString();
