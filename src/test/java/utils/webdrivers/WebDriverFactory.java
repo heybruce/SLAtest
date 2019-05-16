fc21562d673238c3c5fa2cd05abe8cae51e82a72
Bruce Liao <bruce.liao@solera.com>
2019-05-16 14:44:17 +0800

    added parameter os

--- src/test/java/utils/webdrivers/WebDriverFactory.java
+++ src/test/java/utils/webdrivers/WebDriverFactory.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -19,51 +20,7 @@ import java.net.URL;
 
 public class WebDriverFactory {
 
-    private static OptionManager optionManager = new OptionManager();
     private static ThreadLocal<WebDriver> webDriverThreadLocal = new ThreadLocal<>();
-
-//    public static synchronized void setDriver(ITestContext context) {
-//        String browser = context.getCurrentXmlTest().getLocalParameters().get("browser");
-//
-//        if (browser.equalsIgnoreCase("firefox")) {
-//            switch (UtilitiesManager.getSystem()) {
-//                case "Windows" :
-//                    System.setProperty("webdriver.gecko.driver", OptionManager.configProp.getString("geckodriverWin"));
-//                    break;
-//                case "Mac" :
-//                    System.setProperty("webdriver.gecko.driver", OptionManager.configProp.getString("geckodriverMac"));
-//                    break;
-//                case "Linux" :
-//                    System.setProperty("webdriver.gecko.driver", OptionManager.configProp.getString("geckodriverLinux"));
-//                    break;
-//            }
-//            System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE,"/dev/null");
-//
-//            optionManager.setFirefoxOptions(context);
-//            webDriverThreadLocal.set(new FirefoxDriver(optionManager.getFirefoxOptions()));
-//            webDriverThreadLocal.get().manage().window().maximize();
-//        }
-//        else if (browser.equalsIgnoreCase("chrome")) {
-//            switch (UtilitiesManager.getSystem()) {
-//                case "Windows" :
-//                    System.setProperty("webdriver.chrome.driver", OptionManager.configProp.getString("chromedriverWin"));
-//                    break;
-//                case "Mac" :
-//                    System.setProperty("webdriver.chrome.driver", OptionManager.configProp.getString("chromedriverMac"));
-//                    break;
-//                case "Linux" :
-//                    System.setProperty("webdriver.chrome.driver", OptionManager.configProp.getString("chromedriverLinux"));
-//                    break;
-//            }
-//            optionManager.setChromeOptions(context);
-//            webDriverThreadLocal.set(new ChromeDriver(optionManager.getChromeOptions()));
-//            webDriverThreadLocal.get().manage().window().maximize();
-//        } else if (browser.equalsIgnoreCase("ie") && UtilitiesManager.getSystem().equalsIgnoreCase("Windows")) {
-//            System.setProperty("webdriver.ie.driver", OptionManager.configProp.getString("iedriverWin"));
-//            webDriverThreadLocal.set(new InternetExplorerDriver(optionManager.getIeOptions()));
-//            webDriverThreadLocal.get().manage().window().maximize();
-//        }
-//    }
 
     public static synchronized WebDriverWait getWait(WebDriver driver) {
         return new WebDriverWait(driver, 20);
@@ -76,38 +33,46 @@ public class WebDriverFactory {
     public static synchronized void setDriver(ITestContext context) {
         String browser = context.getCurrentXmlTest().getLocalParameters().get("browser");
         String os = context.getCurrentXmlTest().getLocalParameters().get("os");
+        String locale = context.getCurrentXmlTest().getLocalParameters().get("locale");
 
         if (browser.equalsIgnoreCase("firefox")) {
             System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "/dev/null");
             try {
-                DesiredCapabilities desiredCapabilities = DesiredCapabilities.firefox();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                //default set to WINDOWS
+                firefoxOptions.setCapability("platform", Platform.WINDOWS);
+
                 if (os.equalsIgnoreCase("mac")) {
-                    desiredCapabilities.setPlatform(Platform.MAC);
+                    firefoxOptions.setCapability("platform", Platform.MAC);
                 }
                 if (os.equalsIgnoreCase("windows")) {
-                    desiredCapabilities.setPlatform(Platform.WINDOWS);
+                    firefoxOptions.setCapability("platform", Platform.WINDOWS);
                 }
                 if (os.equalsIgnoreCase("any")) {
-                    desiredCapabilities.setPlatform(Platform.ANY);
+                    firefoxOptions.setCapability("platform", Platform.ANY);
                 }
-                webDriverThreadLocal.set(new RemoteWebDriver(new URL("http://10.29.25.100:4444/wd/hub"), desiredCapabilities));
+                webDriverThreadLocal.set(new RemoteWebDriver(new URL("http://10.29.25.100:4444/wd/hub"), firefoxOptions));
                 webDriverThreadLocal.get().manage().window().maximize();
             } catch (Exception e) {
                 e.printStackTrace();
             }
         } else if (browser.equalsIgnoreCase("chrome")) {
             try {
-                DesiredCapabilities desiredCapabilities = DesiredCapabilities.chrome();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--lang=" + locale);
+                //default set to WINDOWS
+                chromeOptions.setCapability("platform", Platform.WINDOWS);
+
                 if (os.equalsIgnoreCase("mac")) {
-                    desiredCapabilities.setPlatform(Platform.MAC);
+                    chromeOptions.setCapability("platform", Platform.MAC);
                 }
                 if (os.equalsIgnoreCase("windows")) {
-                    desiredCapabilities.setPlatform(Platform.WINDOWS);
+                    chromeOptions.setCapability("platform", Platform.WINDOWS);
                 }
                 if (os.equalsIgnoreCase("any")) {
-                    desiredCapabilities.setPlatform(Platform.ANY);
+                    chromeOptions.setCapability("platform", Platform.ANY);
                 }
-                webDriverThreadLocal.set(new RemoteWebDriver(new URL("http://10.29.25.100:4444/wd/hub"), desiredCapabilities));
+                webDriverThreadLocal.set(new RemoteWebDriver(new URL("http://10.29.25.100:4444/wd/hub"), chromeOptions));
                 webDriverThreadLocal.get().manage().window().maximize();
             } catch (Exception e) {
                 e.printStackTrace();
