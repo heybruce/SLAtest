dc5edfc8ab94895efcacde2658e8ebd382531acc
Bruce Liao <bruce.liao@solera.com>
2019-05-17 14:46:28 +0800

    minor refactoring

--- src/test/java/utils/webdrivers/WebDriverFactory.java
+++ src/test/java/utils/webdrivers/WebDriverFactory.java
@@ -1,25 +1,19 @@
 package utils.webdrivers;
 
-import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestContext;
-import utils.UtilitiesManager;
 
-import java.io.File;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class WebDriverFactory {
 
+//    private static OptionManager optionManager = new OptionManager();
     private static ThreadLocal<WebDriver> webDriverThreadLocal = new ThreadLocal<>();
 
     public static synchronized WebDriverWait getWait(WebDriver driver) {
@@ -33,12 +27,37 @@ public class WebDriverFactory {
     public static synchronized void setDriver(ITestContext context) {
         String browser = context.getCurrentXmlTest().getLocalParameters().get("browser");
         String os = context.getCurrentXmlTest().getLocalParameters().get("os");
-        String locale = context.getCurrentXmlTest().getLocalParameters().get("locale");
+        String langLocale = context.getCurrentXmlTest().getLocalParameters().get("locale");
+        String language = langLocale.substring(0,2);
+        String locale = langLocale.substring(3,5);
 
         if (browser.equalsIgnoreCase("firefox")) {
+//            switch (UtilitiesManager.getSystem()) {
+//                case "Windows":
+//                    System.setProperty("webdriver.gecko.driver", OptionManager.configProp.getString("geckodriverWin"));
+//                    break;
+//                case "Mac":
+//                    System.setProperty("webdriver.gecko.driver", OptionManager.configProp.getString("geckodriverMac"));
+//                    break;
+//                case "Linux":
+//                    System.setProperty("webdriver.gecko.driver", OptionManager.configProp.getString("geckodriverLinux"));
+//                    break;
+//            }
+//            System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "/dev/null");
+//            try {
+//                optionManager.setFirefoxOptions(context);
+//                webDriverThreadLocal.set(new FirefoxDriver(optionManager.getFirefoxOptions()));
+//                webDriverThreadLocal.get().manage().window().maximize();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+
+            //Selenium Grid
             System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "/dev/null");
             try {
                 FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.setCapability("language", language);
+                firefoxOptions.setCapability("locale", locale.toUpperCase());
                 //default set to WINDOWS
                 firefoxOptions.setCapability("platform", Platform.WINDOWS);
 
@@ -51,15 +70,38 @@ public class WebDriverFactory {
                 if (os.equalsIgnoreCase("any")) {
                     firefoxOptions.setCapability("platform", Platform.ANY);
                 }
+                //hub
                 webDriverThreadLocal.set(new RemoteWebDriver(new URL("http://10.29.25.100:4444/wd/hub"), firefoxOptions));
                 webDriverThreadLocal.get().manage().window().maximize();
             } catch (Exception e) {
                 e.printStackTrace();
             }
         } else if (browser.equalsIgnoreCase("chrome")) {
+
+//            switch (UtilitiesManager.getSystem()) {
+//                case "Windows":
+//                    System.setProperty("webdriver.chrome.driver", OptionManager.configProp.getString("chromedriverWin"));
+//                    break;
+//                case "Mac":
+//                    System.setProperty("webdriver.chrome.driver", OptionManager.configProp.getString("chromedriverMac"));
+//                    break;
+//                case "Linux":
+//                    System.setProperty("webdriver.chrome.driver", OptionManager.configProp.getString("chromedriverLinux"));
+//                    break;
+//            }
+//            try {
+//                optionManager.setChromeOptions(context);
+//                webDriverThreadLocal.set(new ChromeDriver(optionManager.getChromeOptions()));
+//                webDriverThreadLocal.get().manage().window().maximize();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+
+            //Selenium Grid
             try {
                 ChromeOptions chromeOptions = new ChromeOptions();
-                chromeOptions.addArguments("--lang=" + locale);
+                chromeOptions.setCapability("language", language);
+                chromeOptions.setCapability("locale", locale.toUpperCase());
                 //default set to WINDOWS
                 chromeOptions.setCapability("platform", Platform.WINDOWS);
 
@@ -72,6 +114,7 @@ public class WebDriverFactory {
                 if (os.equalsIgnoreCase("any")) {
                     chromeOptions.setCapability("platform", Platform.ANY);
                 }
+                //hub
                 webDriverThreadLocal.set(new RemoteWebDriver(new URL("http://10.29.25.100:4444/wd/hub"), chromeOptions));
                 webDriverThreadLocal.get().manage().window().maximize();
             } catch (Exception e) {
